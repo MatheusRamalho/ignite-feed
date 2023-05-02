331216ebcd191e157471589b7b69dcf1afa4bca0
Matheus Ramalho <theuramalho@gmail.com>
2023-05-01 21:52:28 -0300

    [main] - feat: adicionando modal para exclusão

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,7 +1,42 @@
-import styles from 'Modal.module.css'
+import styles from './Modal.module.css'
 
-export const Modal = () => {
+type ModalProps = {
+    title: string;
+    description: string;
+
+    isOpenModal: boolean;
+    onModalClose: () => void;
+    onModalDeleteComment: (content: string) => void;
+    contentComment: string;
+}
+
+export const Modal = ({ title, description, isOpenModal = false, onModalClose, onModalDeleteComment, contentComment }: ModalProps) => {
+    const handleDeleteComment = () => {
+        onModalDeleteComment(contentComment);
+    }
+
     return (
-        <div className={styles.modal}></div>
+        <div className={`${styles.modal} ${isOpenModal && styles.modalOpen}`}>
+            <div className={styles.modalContent}>
+                <h3> {title} </h3>
+                <p> {description} </p>
+
+                <div>
+                    <button
+                        type="button"
+                        onClick={onModalClose}
+                    >
+                        Cancelar
+                    </button>
+
+                    <button
+                        type="button"
+                        onClick={handleDeleteComment}
+                    >
+                        Sim, excluir
+                    </button>
+                </div>
+            </div>
+        </div>
     );
 }
--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -7,10 +7,15 @@ import { Comment } from '../Comment'
 import { Avatar } from '../Avatar'
 
 import styles from './Post.module.css'
+import { Modal } from '../Modal'
 
 export const Post = ({ post }: PostProps) => {
     const [comments, setComments] = useState(['Post muito bacana']);
     const [newCommentText, setNewCommentText] = useState('');
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [modalCommentContent, setModalCommentContent] = useState('');
+
     const isNewCommentEmpty = newCommentText.length === 0;
 
     const publishedDateFormatted = format(post.publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
@@ -44,88 +49,109 @@ export const Post = ({ post }: PostProps) => {
         });
 
         setComments(commentsWithoutDeletedOne);
+        setIsModalOpen(false);
+    }
+
+    const handleModalOpen = (commentToDelete: string) => {
+        setModalCommentContent(commentToDelete);
+        setIsModalOpen(true);
+    }
+
+    const handleModalClose = () => {
+        setIsModalOpen(false);
     }
 
     return (
-        <article className={styles.post}>
-            <header className={styles.postHeader}>
-                <div className={styles.headerAuthor}>
-                    <Avatar src={post.author.avatarUrl} />
+        <>
+            <article className={styles.post}>
+                <header className={styles.postHeader}>
+                    <div className={styles.headerAuthor}>
+                        <Avatar src={post.author.avatarUrl} />
 
-                    <div className={styles.authorInfo}>
-                        <strong> {post.author.name} </strong>
-                        <span> {post.author.role} </span>
+                        <div className={styles.authorInfo}>
+                            <strong> {post.author.name} </strong>
+                            <span> {post.author.role} </span>
+                        </div>
                     </div>
+
+                    <time
+                        className={styles.headerPublish}
+                        title={publishedDateFormatted}
+                        dateTime={post.publishedAt.toISOString()}
+                    >
+                        {publishedDateRelativeToNow}
+                    </time>
+                </header>
+
+                <div className={styles.postContent}>
+                    {post.content.map(line => {
+                        if (line.type === 'paragraph') {
+                            return (
+                                <p key={line.content}> {line.content} </p>
+                            );
+
+                        } else if (line.type === 'link') {
+                            return (
+                                <p key={line.content}>
+                                    <a href="#"> {line.content} </a>
+                                </p>
+                            );
+                        }
+                    })}
+
+                    <p>
+
+                        <a href="#"> #nlw </a>
+                        <a href="#"> #rocketseat </a>
+                    </p>
                 </div>
 
-                <time
-                    className={styles.headerPublish}
-                    title={publishedDateFormatted}
-                    dateTime={post.publishedAt.toISOString()}
+                <form
+                    className={styles.postComment}
+                    onSubmit={handleCreateNewComment}
                 >
-                    {publishedDateRelativeToNow}
-                </time>
-            </header>
+                    <label> Deixe seu comentário </label>
 
-            <div className={styles.postContent}>
-                {post.content.map(line => {
-                    if (line.type === 'paragraph') {
+                    <textarea
+                        name="comment"
+                        placeholder="Deixe um comentário..."
+                        value={newCommentText}
+                        onChange={handleNewCommentChange}
+                        onInvalid={handleNewCommentInvalid}
+                        required
+                    ></textarea>
+
+                    <footer>
+                        <button
+                            type="submit"
+                            disabled={isNewCommentEmpty}
+                        >
+                            Publicar
+                        </button>
+                    </footer>
+                </form>
+
+                <div className={styles.commentList}>
+                    {comments.map(comment => {
                         return (
-                            <p key={line.content}> {line.content} </p>
+                            <Comment
+                                key={comment}
+                                content={comment}
+                                onDeleteComment={() => handleModalOpen(comment)}
+                            />
                         );
+                    })}
+                </div>
+            </article>
 
-                    } else if (line.type === 'link') {
-                        return (
-                            <p key={line.content}>
-                                <a href="#"> {line.content} </a>
-                            </p>
-                        );
-                    }
-                })}
-
-                <p>
-
-                    <a href="#"> #nlw </a>
-                    <a href="#"> #rocketseat </a>
-                </p>
-            </div>
-
-            <form
-                className={styles.postComment}
-                onSubmit={handleCreateNewComment}
-            >
-                <label> Deixe seu comentário </label>
-
-                <textarea
-                    name="comment"
-                    placeholder="Deixe um comentário..."
-                    value={newCommentText}
-                    onChange={handleNewCommentChange}
-                    onInvalid={handleNewCommentInvalid}
-                    required
-                ></textarea>
-
-                <footer>
-                    <button
-                        type="submit"
-                        disabled={isNewCommentEmpty}
-                    >
-                        Publicar
-                    </button>
-                </footer>
-            </form>
-
-            <div className={styles.commentList}>
-                {comments.map(comment => {
-                    return (
-                        <Comment
-                            key={comment}
-                            content={comment}
-                            onDeleteComment={deleteComment}
-                        />
-                    );
-                })}
-            </div>
-        </article>
+            <Modal
+                title="Excluir comentário"
+                description="Você tem certeza que gostaria de excluir este comentário?"
+                isOpenModal={isModalOpen}
+                onModalClose={handleModalClose}
+                onModalDeleteComment={deleteComment}
+                contentComment={modalCommentContent}
+            />
+        </>
     );
 }
